259e35af882e421f68105f536f67caafb0f249b3
Safal Adhikari <mr.safaladh@gmail.com>
2020-09-20 11:29:22 +0545

    Learned more about Constructor

--- CSharpBasics/Constructor.cs
+++ CSharpBasics/Constructor.cs
@@ -2,21 +2,25 @@ namespace AllAboutClasses
 {
     public class Country
     {
-        //Default constructor
         public string Name{ get; set; }
         public int Population { get; set; }
         public readonly double AREA = 43443.45;
 
+        public static readonly bool IsOnEarth;  //static means that it's common in all the constructor but when you use "const" it's automatically static
+
+       
+        //Default constructor
         public Country()
         {
            
         }
 
+        
+        // Parameterized constructor
         public Country(string name)
         {
            Name = name;
         }
-        // Parameterized constructor
         public Country(string name, int population, double area)
         {
             Name = name;
@@ -25,14 +29,26 @@ namespace AllAboutClasses
 
         }
 
+        //Static Constructor
+        static Country()
+        {
+            IsOnEarth = true;
+        }
+
+        //Finalizer or Destructor
+        ¬Country()
+        {
+            Consloe.Writeline("I'm dying...");
+        }
+
     }
 
     public class Demo
     {
         void DoSomething()
         {
-            Country country = new Country();
-            Country country1 = new Country("Nepal");
+            Country country1 = new Country();
+            Country country2 = new Country("Nepal");
 
         }
     }
