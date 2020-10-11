9c3648213e1dae71a054cb73c374d09a0ed81bc1
Safal Adhikari <mr.safaladh@gmail.com>
2020-10-11 12:35:47 +0545

    App Modified

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -8,16 +8,21 @@ public class EmployeeController : Controller
 
         // Object initializer syntax
         Person emp1 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp2 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp3 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp4 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp5 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp6 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp7 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp2 = new Person() { FirstName = "James", Surname = "Bond", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp3 = new Person() { FirstName = "John", Surname = "Wick", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp4 = new Person() { FirstName = "Ram", Surname = "Shree", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp5 = new Person() { FirstName = "Kurt", Surname = "Angle", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp6 = new Person() { FirstName = "William", Surname = "Shakespeare", Address = "Bhaktapur", Salary = 2000.0 };
+        Person emp7 = new Person() { FirstName = "Jason", Surname = "Statham", Address = "Bhaktapur", Salary = 2000.0 };
 
         List<Person> employees = new List<Person>() { emp1, emp2,emp3, emp4, emp5, emp6, emp7 };
 
         return View(employees);
+    }
+
+    public ActionResult Detail( string firstName)
+    {
+        return View();
     }
 }
 
