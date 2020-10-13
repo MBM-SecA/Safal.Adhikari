c1e3d5e6457c55af524c5268361f84dcded2eb58
Safal Adhikari <mr.safaladh@gmail.com>
2020-10-13 12:34:54 +0545

    Details list of employee added

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,28 +1,22 @@
-using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-
-        // Object initializer syntax
-        Person emp1 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp2 = new Person() { FirstName = "James", Surname = "Bond", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp3 = new Person() { FirstName = "John", Surname = "Wick", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp4 = new Person() { FirstName = "Ram", Surname = "Shree", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp5 = new Person() { FirstName = "Kurt", Surname = "Angle", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp6 = new Person() { FirstName = "William", Surname = "Shakespeare", Address = "Bhaktapur", Salary = 2000.0 };
-        Person emp7 = new Person() { FirstName = "Jason", Surname = "Statham", Address = "Bhaktapur", Salary = 2000.0 };
-
-        List<Person> employees = new List<Person>() { emp1, emp2,emp3, emp4, emp5, emp6, emp7 };
-
+        var employees = Person.GetEmployees();
+       
         return View(employees);
     }
 
     public ActionResult Detail( string firstName)
     {
-        return View();
+        var employees = Person.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
+
+        return View(employee);
     }
 }
+
 
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 public class Person
 {
     public string FirstName {get; set; }
@@ -5,4 +6,19 @@ public class Person
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
+
+    public static List<Person> GetEmployees()
+    {
+         // Object initializer syntax
+        Person emp1 = new Person() { FirstName = "Safal", Surname = "Adhikari", Address = "Bhaktapur", Salary = 20090.0 };
+        Person emp2 = new Person() { FirstName = "James", Surname = "Bond", Address = "KTM", Salary = 12000.0 };
+        Person emp3 = new Person() { FirstName = "John", Surname = "Wick", Address = "Bhaktapur", Salary = 223000.0 };
+        Person emp4 = new Person() { FirstName = "Ram", Surname = "Shree", Address = "BKT", Salary = 203700.0 };
+        Person emp5 = new Person() { FirstName = "Kurt", Surname = "Angle", Address = "Lalitpur", Salary = 19000.0 };
+        Person emp6 = new Person() { FirstName = "William", Surname = "Shakespeare", Address = "Pokhara", Salary = 45000.0 };
+        Person emp7 = new Person() { FirstName = "Jason", Surname = "Statham", Address = "Kavre", Salary = 62000.0 };
+
+        List<Person> employees = new List<Person> {emp1, emp2, emp3, emp4, emp5, emp6, emp7};
+        return employees;
+    }
 }
