e58677f084f0dae1937b7969c0c61f792a471819
Safal Adhikari <mr.safaladh@gmail.com>
2020-12-05 19:06:00 +0545

    CRUD Completed

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
-    private readonly EMSContext db;
+    private EMSContext db;
 
     public EmployeeController(EMSContext _db)
     {
@@ -18,22 +18,56 @@ public class EmployeeController : Controller
 
     public ActionResult Detail([FromQuery]int id)
     {
-        var employees = Person.GetEmployees();
-        var employee = employees.FirstOrDefault(x => x.Id == id);
-
+        var employee = db.People.Find(id);
         return View(employee);
     }
 
+    [HttpGet]
     public ActionResult Add()
     {
         return View();
     }
 
     [HttpPost]
-    public ActionResult<string> Add(Person person)
+    public ActionResult Add(Person person)
     {
-        return "Record Saved";
+        db.People.Add(person);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
         
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var employee = db.People.Find(id);
+        return View(employee);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Person person)
+    {
+        db.People.Attach(person);
+        db.People.Update(person);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var employee = db.People.Find(id);
+        return View(employee);
+    }
+
+    [HttpPost]
+    public ActionResult Delete(Person person)
+    {
+        db.People.Attach(person);
+        db.People.Remove(person);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
     }
 }
 
